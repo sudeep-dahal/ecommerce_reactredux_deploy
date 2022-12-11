0b50447203777f64cfa63553f2a69bfbec1691e6
Sudeep Dahal <sudeepdahal75@gmail.com>
2022-12-11 21:54:58 +0545

    first commit

---
+++ client/src/components/Slider/Slider.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import EastOutlinedIcon from "@mui/icons-material/EastOutlined";
+import WestOutlinedIcon from "@mui/icons-material/WestOutlined";
+import "./Slider.scss";
+
+const Slider = () => {
+  const data = [
+    "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  ];
+  return (
+    <div className="slider">
+      <div className="container">
+        <img src={data[0]} alt="" />
+        <img src={data[1]} alt="" />
+        <img src={data[2]} alt="" />
+      </div>
+      <div className="icons">
+        <div className="icon">
+          <WestOutlinedIcon />
+        </div>
+        <div className="icon">
+          <EastOutlinedIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
---
+++ client/src/pages/Product/Product.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Product = () => {
+  return <div>Single page</div>;
+};
+
+export default Product;
